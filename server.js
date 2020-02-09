3e16b6372972efa06c3a9d3aa0b8a1bf6cf1d902
saanatwork <saanatwork@gmail.com>
2020-02-09 20:48:38 +0530

    Creation of VM_Vendors Table

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ var mobileApp = require('azure-mobile-apps')(); // Create an instance of a Mobil
 
 mobileApp.tables.add('Post'); // Create a table for 'Book' with default settings
 mobileApp.tables.add('Users');
+mobileApp.tables.add('VM_Vendors')
 
 app.use(mobileApp);
 app.listen(process.env.PORT || 3000);
